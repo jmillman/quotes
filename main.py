e1583ed01237facf6bf640fc99267f402c6534a3
Jared <jared@pcwaiter.com>
2021-11-01 09:34:42 -0400

    .

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 # -*- coding: utf-8 -*-
-
-
 import pandas as pd
-import glob
 import time
 import glob
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from datetime import datetime, timedelta
 import os
+import re
 
-# driver = webdriver.Chrome("./chromedriver")
 
 directory_daily_history = "./stock_history_2021"
 file_name_gap = "./summary/gapped_up-{}-1.csv".format(datetime.now().strftime("%Y-%m-%d"))
 file_name_finviz_summary = 'summary/finviz-{}.csv'.format(datetime.now().strftime("%Y-%m-%d"))
+
+
+
+# CHROMEDRIVER = os.path.abspath("chromedriver.exe")
+# BINARY_LOCATION = os.path.abspath(r"chromium\chrome.exe")
+
+# chrome_options = webdriver.chrome.options.Options()
+# chrome_options.add_argument("--no-sandbox")
+# driver = webdriver.Chrome("./chromedriver", options=chrome_options)
+#
+# For some reason, yahoo loads much faster headless
+# chrome_options2 = webdriver.chrome.options.Options()
+# chrome_options2.add_argument("--headless")
+# driver_yahoo = webdriver.Chrome("./chromedriver", options=chrome_options2)
 
 
 def get_file(symbol, date):
@@ -129,14 +140,96 @@ def get_data_finviz(symbol):
     return results
 
 
-def save_active_stocks_finviz_to_file():
-    symbols_with_volume = sorted(get_symbols_over_million_volume())
+def scrape_yahoo_stats(soup, results, search_item):
+  key = search_item[0]
+  search_text = search_item[1]
+  span_with_text = soup.find_all("span", text=re.compile('.*' + search_text + '.*'))
+  value = 'Not Found'
+  title = ''
+  if span_with_text:
+    title = span_with_text[0].parent.parent.contents[0].get_text()
+    value = span_with_text[0].parent.parent.contents[1].get_text()
+  results[key] = {
+    'title': title,
+    'value': value,
+  }
+  return
 
-    for index, symbol in enumerate(symbols_with_volume):
+
+def get_data_yahoo(symbol):
+  url = 'https://finance.yahoo.com/quote/' + symbol + '/key-statistics'
+
+  driver_yahoo.get(url)
+
+  # this is just to ensure that the page is loaded
+  time.sleep(0)
+
+  html = driver_yahoo.page_source
+
+  soup = BeautifulSoup(html, "html.parser")
+  results = {}
+  search_items = {}
+  search_items['market_cap'] = 'Market Cap '
+  search_items['shares_outstanding'] = 'Shares Outstanding'
+  search_items['float'] = 'Float'
+  search_items['held_by_insiders'] = 'Held by Insiders'
+  search_items['held_by_institutions'] = 'Held by Institutions'
+  search_items['shares_short'] = 'Shares Short'
+  search_items['52_week_change'] = '52-Week Change'
+  search_items['52_week_high'] = '52 Week High'
+  search_items['52_week_low'] = '52 Week Low'
+  search_items['50_day_moving_avg'] = '50-Day Moving Average'
+  search_items['200_day_moving_avg'] = '200-Day Moving Average'
+  search_items['avg_vol_3_mo'] = 'Avg Vol (3 month)'
+  search_items['avg_vol_10_mo'] = 'Avg Vol (10 day)'
+  search_items['shares_outstanding'] = 'Shares Outstanding'
+  search_items['short_ratio'] = 'Short Ratio'
+  search_items['short_percent_of_float'] = 'Short % of Float'
+  search_items['short_percent_of_shares_outstanding'] = 'Short % of Shares Outstanding'
+  for search_item in search_items.items():
+    scrape_yahoo_stats(soup, results, search_item)
+
+  return results
+
+def save_active_stocks_finviz_to_file(update_only_missing_symbols=False):
+    file_name_finviz = "./summary/finviz-2021-10-30.csv"
+    symbols = []
+    if update_only_missing_symbols:
+        gapped = pd.read_csv(file_name_gap)
+        finviz = pd.read_csv(file_name_finviz)
+        for i, row in gapped.iterrows():
+            symbol = row['symbol']
+            finviz_item = finviz[finviz['Symbol'] == symbol]
+            if (len(finviz_item) == 0):
+                gapped.loc[i, 'DATA_MISSING_FINVIZ'] = True
+                symbols.append(symbol)
+        symbols = sorted(set(symbols))
+        print('Missing Symbols = {}'.format(symbols))
+    else:
+        symbols = sorted(set(get_symbols_over_million_volume()))
+
+    for index, symbol in enumerate(symbols):
         finviz = get_data_finviz(symbol)
-        finviz_df = pd.DataFrame(finviz, index=[0])
+        df = None
+        if('Shs Float' not in finviz or finviz['Shs Float'] == '-'):
+            print('Yahoo {}'.format(symbol))
+            yahoo_data = get_data_yahoo(symbol)
+            yahoo = {}
+            yahoo['Symbol'] = symbol
+            yahoo['Market Cap'] = yahoo_data.get('market_cap').get('value')
+            yahoo['Short Float'] = yahoo_data.get('short_percent_of_float').get('value')
+            yahoo['Shs Float'] = yahoo_data.get('float').get('value')
+            yahoo['Shs Outstand'] = yahoo_data.get('shares_outstanding').get('value')
+            yahoo['source'] = 'YAHOO'
+            df = pd.DataFrame(yahoo, index=[0])
+            df.append(yahoo, ignore_index=True)
+        else:
+            print('FinViz {}'.format(symbol))
+            finviz['source'] = 'FINVIZ'
+            df = pd.DataFrame(finviz, index=[0])
+
         showHeaders = True if index == 0 else False
-        finviz_df.to_csv(file_name_finviz_summary, mode='a+', header=showHeaders, index=False)
+        df.to_csv(file_name_finviz, mode='a+', header=showHeaders, index=False)
 
 
 def get_stock_by_date(symbol, date):
@@ -415,6 +508,8 @@ def add_high_low_time_to_gap_up():
 def convert_billion_to_mill(str):
     if str == '-' or pd.isna(str):
         return 0
+    elif 'k' in str:
+        return float(str.replace("k", "")) / 1000
     elif 'B' in str:
         return float(str.replace("B", "")) * 1000
     else:
@@ -422,36 +517,46 @@ def convert_billion_to_mill(str):
 
 
 def add_finviz_to_gap_up():
-    file_name_finviz = "./summary/finviz-2021-07-03.csv"
+    file_name_finviz = "./summary/finviz-2021-10-30.csv"
     gapped = pd.read_csv(file_name_gap)
     finviz = pd.read_csv(file_name_finviz)
     for i, row in gapped.iterrows():
+        missingData = False
         symbol = row['symbol']
         finviz_item = finviz[finviz['Symbol'] == symbol]
+
         if (len(finviz_item)):
-            gapped.loc[i, 'float'] = convert_billion_to_mill(finviz_item.iloc[0]['Shs Float'])
-            shares = convert_billion_to_mill(finviz_item.iloc[0]['Shs Outstand'])
+
+            # If no float, but shares use shares for float and vice versa
+            # typically it's really low shares outstanding so the float has to be less than that
+            float_shrs = finviz_item.iloc[0]['Shs Float']
+            if float_shrs in ['Not Found', 'N/A', '-']:
+                float_shrs = 0
+                missingData = True
+
+            shares = finviz_item.iloc[0]['Shs Outstand']
+            if shares in ['Not Found', 'N/A', '-']:
+                missingData = True
+                shares = float_shrs
+
+            if float_shrs == 0:
+                float_shrs = shares
+
+            if(float_shrs != 0):
+                float_shrs = convert_billion_to_mill(float_shrs)
+            if(shares != 0):
+                shares = convert_billion_to_mill(shares)
+            gapped.loc[i, 'float'] = shares
             gapped.loc[i, 'shares'] = shares
-            gapped.loc[i, 'market_cap'] = "{:.2f}".format(shares * float(row['close_yesterday']))
+            gapped.loc[i, 'market_cap'] = "{:.2f}".format(shares * float(gapped.loc[i, 'close_yesterday']))
         else:
-            gapped.loc[i, 'DATA_MISSING_FINVIZ'] = True
+            missingData = True
             print('Finviz missing symbol: {}'.format(symbol))
 
-    gapped.to_csv(file_name_gap, index=False)
-
-def find_missing_finviz():
-    file_name_finviz = "./summary/finviz-2021-07-03.csv"
-    gapped = pd.read_csv(file_name_gap)
-    finviz = pd.read_csv(file_name_finviz)
-    missing_symbols = []
-    for i, row in gapped.iterrows():
-        symbol = row['symbol']
-        finviz_item = finviz[finviz['Symbol'] == symbol]
-        if (len(finviz_item) == 0):
+        if missingData == True:
             gapped.loc[i, 'DATA_MISSING_FINVIZ'] = True
-            missing_symbols.append(symbol)
-    print('Missing Symbols = {}'.format(missing_symbols))
 
+    gapped.to_csv(file_name_gap, index=False)
 
 
 def add_bool_columns_new(gapped, name):
@@ -476,7 +581,6 @@ def add_bool_columns_new(gapped, name):
     gapped['{}_max_low_10p'.format(name)] = percent_change_low >= .10
     gapped['{}_max_low_25p'.format(name)] = percent_change_low >= .25
     gapped['{}_max_low_50p'.format(name)] = percent_change_low >= .50
-    gapped['{}_max_low_100p'.format(name)] = percent_change_low >= 1
 
     # gapped['{}_percent_both_10p'.format(name)] = bool(percent_change_high >= .10) and bool(percent_change_low >= .10)
     # gapped['{}_percent_both_25p'.format(name)] = bool(percent_change_high >= .25) and bool(percent_change_low >= .25)
@@ -491,6 +595,9 @@ def add_booleans_new():
     for slice in slices:
         add_bool_columns_new(gapped, slice)
 
+    volume = gapped['volume']
+    gapped['rotate'] = float(gapped['volume']) < (float(gapped['float']) * 1000000)
+    gapped['rotate_count'] = float(gapped['volume']) / (float(gapped['float']) * 1000000)
     gapped.to_csv(file_name_gap, index=False)
 
 
@@ -520,6 +627,7 @@ def calculate_stats_from_booleans():
 
 
 if __name__ == "__main__":
+
     # Will save finviz data for any stock that had over a million in volume
     # save_active_stocks_finviz_to_file()
 
@@ -533,12 +641,17 @@ if __name__ == "__main__":
     # add_volume_to_gap_up()
     # add_booleans_to_gap_up()
 
-    #start = datetime(2021, 1, 1).date()
-    #save_gap_up_data_to_summary_file(start, 30)
-    #add_finviz_to_gap_up()
-    #add_high_low_time_to_gap_up()
-    #add_booleans_new()
-    #calculate_stats_from_booleans()
-    find_missing_finviz()
+    start = datetime(2021, 1, 1).date()
+    save_gap_up_data_to_summary_file(start, 20)
+    add_finviz_to_gap_up()
+    add_high_low_time_to_gap_up()
+    add_booleans_new()
+    calculate_stats_from_booleans()
 
+
+
+    # save_active_stocks_finviz_to_file()
+
+    # driver_yahoo = None
+    # driver = None
     print('END')
